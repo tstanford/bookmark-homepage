af6220ada73ae16d436d195d89f666f6b465a5df
tim stanford <tstanford@gmail.com>
2025-10-24 17:46:22 +0100

    Add comment in the shell script to explain what it is

--- updateVars.sh
+++ updateVars.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This script is used in the docker image to dynamically set application environment variables and make them available on the client.
+# See public/vars.js
+
 for var in `env | grep ^BMS_ | cut -d= -f1` 
 do
     echo $var
