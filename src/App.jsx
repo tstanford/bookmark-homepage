0c3fb10223f4849e19972e89a3404ad756f29bb5
tim stanford <tstanford@gmail.com>
2025-10-26 17:56:14 +0000

    Refactor PageHeading and style nicely

--- src/App.jsx
+++ src/App.jsx
@@ -252,38 +252,17 @@ function App() {
 
     return (
         <div>
-
-            <div id="editSwitch">
-                <label className="switch">
-                    <input type="checkbox" checked={editMode} onChange={toggleEditMode} />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-
-            {editMode && 
-                <>
-                    <div className="actionbutton exportfile">
-                        <button role="actionbutton">
-                            <span className="material-symbols-outlined" onClick={downloadExportFile}>download</span>
-                        </button>
-                    </div>
-
-                    <div className="actionbutton importfile">
-                        <input type='file' id='file' ref={inputFile} onChange={uploadImportFile} accept=".yaml" style={{ display: 'none' }} />
-                        <button role="actionbutton">
-                            <span className="material-symbols-outlined" onClick={selectedImportFile}>upload</span>
-                        </button>
-                    </div>
-
-                    <div className="actionbutton deleteAll">
-                        <button role="actionbutton">
-                            <span className="material-symbols-outlined" onClick={deleteAll}>clear_all</span>
-                        </button>
-                    </div>
-                </>
-            }
-
-            <PageHeading date={new Date().toDateString()}></PageHeading>
+            
+            <PageHeading
+                editMode={editMode} 
+                toggleEditMode={toggleEditMode} 
+                downloadExportFile={downloadExportFile}
+                inputFile={inputFile}
+                uploadImportFile={uploadImportFile}
+                selectedImportFile={selectedImportFile}
+                deleteAll={deleteAll}
+            />
+            
             <SearchBox onChange={searchOnChange} onKeyUp={searchOnKeyUp}></SearchBox>
 
             <article id="folders">
--- src/components/pageheading.jsx
+++ src/components/pageheading.jsx
@@ -1,11 +1,40 @@
-export default function PageHeading({ date}) {
+export default function PageHeading({ editMode, toggleEditMode, downloadExportFile, inputFile, uploadImportFile, selectedImportFile, deleteAll }) {
     return (
+        <heading>
+            <div className="logo"></div>
+            <div className="date">{new Date().toDateString()}</div>
 
-        <header>
-            <h1>
-                <div className="titlecase">Tim's Home Page</div>
-                <div className="date">{date}</div>
-            </h1>
-        </header>
+            <div class="buttons">
+                <div id="editSwitch">
+                    <label className="switch">
+                        <input type="checkbox" checked={editMode} onChange={toggleEditMode} />
+                        <span className="slider round"></span>
+                    </label>
+                </div>
+
+                {editMode &&
+                    <>
+                        <div className="actionbutton exportfile">
+                            <button role="actionbutton">
+                                <span className="material-symbols-outlined" onClick={downloadExportFile}>download</span>
+                            </button>
+                        </div>
+
+                        <div className="actionbutton importfile">
+                            <input type='file' id='file' ref={inputFile} onChange={uploadImportFile} accept=".yaml" style={{ display: 'none' }} />
+                            <button role="actionbutton">
+                                <span className="material-symbols-outlined" onClick={selectedImportFile}>upload</span>
+                            </button>
+                        </div>
+
+                        <div className="actionbutton deleteAll">
+                            <button role="actionbutton">
+                                <span className="material-symbols-outlined" onClick={deleteAll}>clear_all</span>
+                            </button>
+                        </div>
+                    </>
+                }
+            </div>
+        </heading>
     )
 }
