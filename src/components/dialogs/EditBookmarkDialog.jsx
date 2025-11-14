c9f72f41cd676f7b389421106ba2a7331bf08669
tim stanford <tstanford@gmail.com>
2025-11-14 21:40:14 +0000

    Add image viewer to edit bookmark dialog

--- src/components/dialogs/EditBookmarkDialog.jsx
+++ src/components/dialogs/EditBookmarkDialog.jsx
@@ -1,22 +1,42 @@
 export default function EditBookmark({dialogRef, bookmark, prepopulatedName, prepopulatedUrl, onDismiss, onSubmit, onChange, onDelete}){
     return(
 
-        <dialog ref={dialogRef}>                
+        <dialog ref={dialogRef} class="bookmark">                
                 <form onSubmit={onSubmit}>
-                    <div className="field">
-                        <label htmlFor="title">Title:</label>
-                        <input id="title" name="title" value={prepopulatedName} onChange={onChange}  />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="url">URL:</label>
-                        <input id="url" name="url" value={prepopulatedUrl} onChange={onChange} />
-                    </div>
 
-                    <div className="buttons">
-                        <button className="flat" onClick={onSubmit}>Apply Changes</button>
-                        <button className="flat" onClick={() => onDelete(bookmark.id)}>Delete</button>
-                        <button className="flat" onClick={onDismiss}>Cancel</button>                        
-                    </div>
+                    {bookmark != null &&
+
+                    <>
+
+                    <section>
+                        <label>Icon:</label>
+                        <div className="icon" style={{"background-image": `url(${bookmark.favicon})`}}/>
+                        <div>
+                            <button>Clear</button>
+                            <button>Upload</button>
+                        </div>
+                    </section>
+                    
+                    <section>                   
+                        <div className="field">
+                            <label htmlFor="title">Title:</label>
+                            <input id="title" name="title" value={prepopulatedName} onChange={onChange}  />
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="url">URL:</label>
+                            <input id="url" name="url" value={prepopulatedUrl} onChange={onChange} />
+                        </div>
+
+                        <div className="buttons">
+                            <button className="flat" onClick={onSubmit}>Apply Changes</button>
+                            <button className="flat" onClick={() => onDelete(bookmark.id)}>Delete</button>
+                            <button className="flat" onClick={onDismiss}>Cancel</button>                        
+                        </div>
+                    </section>
+
+                    </>
+                    }
                 </form>
         </dialog>
 
