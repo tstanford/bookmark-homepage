4cdb07a93e53dd9d73ffd4c67e1d5b5b97cf4a9f
tim stanford <tstanford@gmail.com>
2025-11-15 21:15:26 +0000

    Add some styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './css/spinner.css';
 import './css/slider.css';
 import './css/dialog.css';
 import './css/base.css';
-import './css/blue.css';
 import './css/table.css';
 import './css/folder.css';
+import './css/blue.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
