668bc7709409756559fa8e403635cab36bd66b34
Tim Stanford <tstanford@gmail.com>
2025-11-15 22:13:40 +0000

    Allow the user to change the bookmark icon from the UI (#4)
    * Add image viewer to edit bookmark dialog
    
    * Change width of dialog to 30em
    
    * Add feature to change the icon of a bookmark

--- src/components/dialogs/EditBookmarkDialog.jsx
+++ src/components/dialogs/EditBookmarkDialog.jsx
@@ -1,22 +1,71 @@
-export default function EditBookmark({dialogRef, bookmark, prepopulatedName, prepopulatedUrl, onDismiss, onSubmit, onChange, onDelete}){
+import { useRef } from "react";
+
+export default function EditBookmark({dialogRef, bookmark, prepopulatedName, prepopulatedUrl, onDismiss, onSubmit, onChange, onDelete, uploadIcon}){
+    const imagefile = useRef(null);
+
+    var openImageFileDialog = (ev) => {
+        imagefile.current.click();
+        ev.preventDefault();
+
+    };
+
+    var uploadImage = (ev) => {
+        ev.preventDefault();
+        const file = ev.target.files[0];
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            var importData = e.target.result;
+            console.log(importData);
+            bookmark.favicon = importData;
+            uploadIcon(bookmark, importData);
+        };
+
+        reader.readAsDataURL(file);
+    };
+
+    var clearIcon = (ev) => {
+        ev.preventDefault();
+        uploadIcon(bookmark, "");
+    };
+    
     return(
 
-        <dialog ref={dialogRef}>                
+        <dialog ref={dialogRef} class="bookmark">                
                 <form onSubmit={onSubmit}>
-                    <div className="field">
-                        <label htmlFor="title">Title:</label>
-                        <input id="title" name="title" value={prepopulatedName} onChange={onChange}  />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="url">URL:</label>
-                        <input id="url" name="url" value={prepopulatedUrl} onChange={onChange} />
-                    </div>
 
-                    <div className="buttons">
-                        <button className="flat" onClick={onSubmit}>Apply Changes</button>
-                        <button className="flat" onClick={() => onDelete(bookmark.id)}>Delete</button>
-                        <button className="flat" onClick={onDismiss}>Cancel</button>                        
-                    </div>
+                    {bookmark != null &&
+                    <>
+                        <input type='file' id='imagefile' ref={imagefile} onChange={uploadImage} accept=".png,.svg,.jpg,.gif,.ico" style={{ display: 'none' }} />
+
+                        <section>
+                            <label>Icon:</label>
+                            <div className="icon" style={{"background-image": `url(${bookmark.favicon})`}}/>
+                            <div>
+                                <button onClick={clearIcon}>Clear</button>
+                                <button onClick={openImageFileDialog}>Upload</button>
+                            </div>
+                        </section>
+                        
+                        <section>                   
+                            <div className="field">
+                                <label htmlFor="title">Title:</label>
+                                <input id="title" name="title" value={prepopulatedName} onChange={onChange}  />
+                            </div>
+
+                            <div className="field">
+                                <label htmlFor="url">URL:</label>
+                                <input id="url" name="url" value={prepopulatedUrl} onChange={onChange} />
+                            </div>
+
+                            <div className="buttons">
+                                <button className="flat" onClick={onSubmit}>Apply Changes</button>
+                                <button className="flat" onClick={() => onDelete(bookmark.id)}>Delete</button>
+                                <button className="flat" onClick={onDismiss}>Cancel</button>                        
+                            </div>
+                        </section>
+                    </>
+                    }
                 </form>
         </dialog>
 
